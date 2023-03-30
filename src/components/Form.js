03f1abeb69ca86d62d6d01f0439c4d847f7afa0e
SharmaTushar <sharmahimesh282@gmail.com>
2023-03-30 09:28:23 +0530

    added form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
 import "./Form.css"
-import React from 'react'
+import React , { useRef }from 'react'
 import { useState  } from "react";
 import { useNavigate } from "react-router-dom";
+
+import emailjs from '@emailjs/browser';
 
 
 const Form = () => {
@@ -12,27 +14,35 @@ const Form = () => {
   const[message ,setMessage] = useState("");
   const[nameerr ,setNameerr] = useState(true);
   const[emailerr ,setEmailerr] = useState(true);
-
+  const form = useRef();
   const submit = async(e)=>{
     e.preventDefault();
 //     const userId =JSON.parse(localStorage.getItem('details')) ;
 //      console.warn(userId._id);
    
-   let result = await fetch('http://localhost:4500/save',{
-method:'post',
-body:JSON.stringify({name,email,subject,message}),
-headers:{
-'Content-Type':'application/json'
-}, 
-})
-  result = await result.json();
-console.warn(result);
-localStorage.setItem("user",JSON.stringify(result));
-if(result){
- navigate('/')
-    alert("your message is submitted");
+//    let result =  fetch('http://localhost:4500/save',{
+// method:'post',
+// body:JSON.stringify({name,email,subject,message}),
+// headers:{
+// 'Content-Type':'application/json'
+// }, 
+// })
+//   result = await result.json();
+// console.warn(result);
+// localStorage.setItem("user",JSON.stringify(result));
 
-}
+emailjs.sendForm('service_bvj4aaw', 'template_i4deg5l', form.current, 'aTQ94qnQ-QcU5JC3s')
+.then((result) => {
+    console.log(result.text);
+     alert("your message is submitted"); 
+     navigate('/')
+
+}, (error) => {
+    console.log(error.text);
+});
+
+
+   
      
   }
 
@@ -62,20 +72,20 @@ let item = e.target.value;
    }
   return (
     <div className="form">
-        <form onSubmit={submit} >
+        <form onSubmit={submit} ref={form} >
         <label htmlFor="">Your Name</label>
-        <input type="text" className="text" value={name} onChange={namesubmit}/>
+        <input type="text"  name="name"  className="text" value={name} onChange={namesubmit}/>
         {nameerr?<p style={{color:"red"}}>enter correct details  !</p>:""}
      
         <label htmlFor="">Email</label>
-      <input type="email" className="text" value={email}  onChange={emailsubmit}/>
+      <input type="email" name="email" className="text" value={email}  onChange={emailsubmit}/>
       {emailerr?<p style={{color:"red"}}>enter correct details  !</p>:""}
       <label htmlFor="">Subject</label>
       <input type="text" className="text"value={subject} onChange={(e)=>setSubject(e.target.value)}/>
      
       
       <label htmlFor="">Message</label>
-      <textarea placeholder="Enter Your Message Here" id="" cols="30" rows="3" className="text" value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
+      <textarea  name="message" placeholder="Enter Your Message Here" id="" cols="30" rows="3" className="text" value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
       <button className="btn">SUBMIT</button>
       </form>
     </div>
