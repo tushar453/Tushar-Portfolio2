6c5baa939a22b6ee9cab59d356b7c4b01f59bc23
SharmaTushar <sharmahimesh282@gmail.com>
2023-03-30 12:09:24 +0530

    added preloader

--- src/App.js
+++ src/App.js
@@ -5,49 +5,76 @@ import Home from './Routes/Home';
 import Contact from './Routes/Contact';
 import About from './Routes/About';
 import Project from './Routes/Project';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 
 function App() {
+
+  const[loading,setLoading] = useState(false);
+
   useEffect(()=>{
+     setLoading(true)
+     setTimeout(()=>{
+  setLoading(false)
+     
+
+     },5000) 
 
 
-
-
+    
 
 
  
-    
-  var cursor = document.querySelector('.cursor');
-  var cursor2  = document.querySelector('.cursor2');
-  document.addEventListener("mousemove" ,function(e){
- cursor.style.cssText= cursor2.style.cssText= "left:"+e.clientX +"px;top:"
- + e.clientY+"px;";
-  });
+  
     },[])
 
+   useEffect(()=>{
 
+    var cursor = document.querySelector('.cursor');
+    var cursor2  = document.querySelector('.cursor2');
+    document.addEventListener("mousemove" ,function(e){
+   cursor.style.cssText= cursor2.style.cssText= "left:"+e.clientX +"px;top:"
+   + e.clientY+"px;";
+    });
+   },[])
 
   return (
-   <div>
-       <Router>
+    
+<div>
+     {
+      loading ?
+      <ClimbingBoxLoader className='spinner' color="rgb(248,217, 15)" size={30} />
+      :
+
+
+      <div>
+      <Router>
        
-     <Routes>
-      <Route path="/" exact element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/project" element={<Project/>}/>
-     
-      </Routes>
-    </Router>
-  
-
-
-    <div class="cursor"></div>
-    <div class="cursor2"></div>
-  
-  
+      <Routes>
+       <Route path="/" exact element={<Home/>}/>
+       <Route path="/about" element={<About/>}/>
+       <Route path="/contact" element={<Contact/>}/>
+       <Route path="/project" element={<Project/>}/>
+      
+       </Routes>
+     </Router>
+   
+ 
+ 
+    
    </div>
+   
+     }
+
+<div class="cursor"></div>
+     <div class="cursor2"></div>
+</div>
+
+
+      
+
+   
   );
 }
 
