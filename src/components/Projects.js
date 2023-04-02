7147489b0ef0cfaa164362bfb49dfcc786e2e5bd
SharmaTushar <sharmahimesh282@gmail.com>
2023-04-02 19:29:26 +0530

    added project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,8 +8,10 @@ const Projects = () => {
     <>
 
     <div className="project">
-        <h2><span>P</span>age  <span>Comm</span>ing Soon</h2>      
-           
+        {/* <h2><span>P</span>age  <span>Comm</span>ing Soon</h2>       */}
+        <div className="projects"></div>
+           <h1>Weather App</h1>
+           <li><NavLink to="https://weather-app-eight-teal.vercel.app/" target="_blank">Click Here</NavLink></li>
     </div>
     
     </>
