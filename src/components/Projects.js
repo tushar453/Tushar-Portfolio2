868b3d411ca70420f8fcd17ec905f8a14539e442
SharmaTushar <sharmahimesh282@gmail.com>
2023-08-23 20:53:43 +0530

    added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,16 +3,37 @@ import { NavLink } from "react-router-dom";
 import Introi from "../components/assests/nit.jpeg";
 // import Intr from "../components/assests/project.jpeg";
 import "./Projects.css";
+import resume from "./assests/Tushar-Resume.pdf";
+
+import Heart from "./assests/png1.png"
+
 const Projects = () => {
   return (
     <>
+    <div className="projects">
+      <div className="left-side">
+      <span>MY AWESOME</span>
+      <br/>
+      <span>PROJECTS</span>
+      <br/>
+      <span><p>Checkout My Awesome Projects and Resume</p></span>
+   <a href={resume} download><button className="btn buttonss">Resume</button></a>
 
-    <div className="project">
-        {/* <h2><span>P</span>age  <span>Comm</span>ing Soon</h2>       */}
-        <div className="projects"></div>
-           <h1>Weather App</h1>
-           <li><NavLink to="https://nreact-weather-app.vercel.app/" target="_blank">Click Here</NavLink></li>
-    </div>
+      </div>
+      <div className="right-side"></div>
+      <div className="right-card">
+        <span>
+          react-weather-app
+        </span>
+
+        <span>details</span>
+        <button>wwedew</button>
+      </div>
+      </div> 
+
+
+
+
     
     </>
   );
