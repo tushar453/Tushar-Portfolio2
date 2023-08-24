b098f1e5e7ffb8a97a60907ac0c663bcf7134ab8
SharmaTushar <sharmahimesh282@gmail.com>
2023-08-24 21:30:13 +0530

    aded front

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,66 +1,46 @@
 import "./Hero.css";
 import React, { useEffect } from "react";
 import Introimg from "../components/assests/heroimg.jpg";
-
+import svg from "./assests/marsh.svg";
 
 import { NavLink } from "react-router-dom";
 import Typewriter from "typewriter-effect";
 // import Particles from "react-tsparticles";
 import particlesConfig from "./particles-config";
-import Particles from './Particles';
+import Particles from "./Particles";
 import { Background } from "tsparticles-engine";
 const Hero = () => {
-
-
-
-
-
-  
-  
   return (
-    
-      
-    <div className="hero" >
+    <div className="hero">
       <div className="mask">
-
-       
-      </div>
-      <div className="content">
-        <p>
-          {" "}
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .typeString("Hi This Is TusharKant Sharma")
-
-                .pauseFor(1000)
-                .deleteAll()
-                .typeString("I'm a  FullStack Developer.")
-                .start();
-            }}
-          />
-        </p>
-
-        <div>
-          <NavLink to="/project" className="btn">
-            Projects{" "}
-          </NavLink>
-          <NavLink to="/contact" className="btn btn-light">
-            Contact
-          </NavLink>
-        </div>
-
-
-       
-      </div>
-      
-
-      <Particles id="tsparticles" />
-
-    </div> 
     
-   
-   
+    <div className="sections">
+     <div className="up">
+          <img
+            className="svgs"
+            src={require("./assests/marsh.svg").default}
+            alt="/"/>
+         </div> 
+
+         <div className="down">
+             <p>
+            {" "}
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Hi This Is TusharKant Sharma")
+      
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("I'm a  FullStack Developer.")
+                  .start();
+              }}
+            />
+          </p></div>          
+     </div>
+    </div>
+      <Particles id="tsparticles" />
+    </div>
   );
 };
 
